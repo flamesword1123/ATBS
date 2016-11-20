e49e06b770646cd6ae2bebcca405a4e2113d1fab
Jacob Bernheim <bernheimjacob@gmail.com>
2016-11-20 17:47:11 -0500

    edits for convention

--- passwordLocker.py
+++ passwordLocker.py
@@ -1,21 +1,21 @@
 #! /usr/bin/env python3
 # Password Locker - An insecure password locker program from ATBS Chapter 6
-
-PASSWORDS = {"email" : "random@mail.something",
-			 "blog": "sdfghjkasdjklkjhgfds",
-			 "luggage": "12345765432"}
-
-
 import sys
 import pyperclip
+
+PASSWORDS = {"email" : "random@mail.something",
+             "blog": "sdfghjkasdjklkjhgfds",
+             "luggage": "12345765432"}
+
+
 if len(sys.argv) < 2:
-	print("Usage: python passwordLocker.py [account] - copy account password")
-	sys.exit()
+    print("Usage: python passwordLocker.py [account] - copy account password")
+    sys.exit()
 
 account = sys.argv[1]	# first command line arg is the account name
 
 if account in PASSWORDS:
-	pyperclip.copy(PASSWORDS[account])
-	print("Password for " + account + " copied to clipboard.")
+    pyperclip.copy(PASSWORDS[account])
+    print("Password for " + account + " copied to clipboard.")
 else:
-	print("There is no account named " + account)
+    print("There is no account named " + account)
